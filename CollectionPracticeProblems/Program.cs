f3be0eabfd971ae7e55868c92abaf20924631236
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-30 18:15:59 +0530

    Add: File

---
+++ CollectionPracticeProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                    NChapters chaptersCount = new NChapters();
+                    chaptersCount.RemainingChapters();
+        
+        }
+    }
+}
